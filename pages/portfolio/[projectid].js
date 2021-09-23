4dd4382875409905261fd5f7cca244eeda744c13
Khoi Dao <khoidao55@gmail.com>
2021-09-23 16:05:49 -0700

    dynamic link component for dynamic routes

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 const PortfolioProjectPage = () => {
   const router = useRouter();
 
+  console.log(router.query);
   return (
     <div>
       <h1>Portfolio Project Item</h1>
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+
 const PortfolioPage = () => {
+  const projects = [
+    {id: 'robinhood', name: 'Robinhood'},
+    {id: 'photography', name: 'Photofy'},
+    {id: 'nodolla', name: 'NoDolla'}
+  ]
+
   return (
     <div>
       <h1>Portfolio Page</h1>
+      <ul>
+        {projects.map(project => (
+        <li key={project.id}>
+          <Link href={`/portfolio/${project.id}`}>{project.name}</Link>
+        </li>))}
+      </ul>
     </div>
   )
 }
